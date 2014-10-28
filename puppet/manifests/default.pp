76593c1bc2e3d0d4592a13ef54bf1af9e7680966
Nerijus Vaiciulis <nerijus.vaiciulis@nfq.lt>
2014-10-28 15:45:21 +0200

    Vagrant update

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -3,6 +3,31 @@ Exec {
 }
 
 class bootstrap {
+  # node
+  apt::source { 'debian_sid':
+    location          => 'http://ftp.lt.debian.org/debian/',
+    release           => 'sid',
+    repos             => 'main contrib non-free',
+    required_packages => 'debian-keyring debian-archive-keyring',
+    key               => '46925553',
+    key_server        => 'subkeys.pgp.net',
+    pin               => '1',
+    include_src       => true
+  }
+
+  # libc6
+  apt::source { 'debian_backports':
+    location          => 'http://ftp.litnet.lt/debian/',
+    release           => 'wheezy-backports',
+    repos             => 'main contrib non-free',
+    required_packages => 'debian-keyring debian-archive-keyring',
+    key               => '46925553',
+    key_server        => 'subkeys.pgp.net',
+    pin               => '700',
+    include_src       => true
+  }
+
+  # php 5.6
   apt::source { 'packages.dotdeb.org-php':
     location          => 'http://packages.dotdeb.org',
     release           => 'wheezy-php56',
@@ -16,16 +41,17 @@ class bootstrap {
 }
 
 stage { "init":    before  => Stage["main"] }
-stage { "cleanup": require => Stage["main"] }
+stage { "post":    require => Stage["main"] }
 
-class {'bootstrap':
+class {"bootstrap":
   stage => init
 }
 
-class { 'apt':
+class { "apt":
   stage => init
 }
 
+# apache
 package { "apache2":
   ensure  => present,
   require => Exec["apt_update"]
@@ -71,11 +97,13 @@ service { "apache2":
   ]
 }
 
+# mysql
 package { "mysql-server":
   ensure => present,
   require => Exec["apt_update"]
 }
 
+#php
 $php_required = [
   "php5",
   "php5-cli",
@@ -89,7 +117,7 @@ $php_required = [
 ]
 
 $php_purge = [
-  'php5-suhosin'
+  "php5-suhosin"
 ]
 
 package { $php_required:
@@ -99,7 +127,7 @@ package { $php_required:
 
 package { $php_purge:
   ensure  => purged,
-  require => Package['php5'],
+  require => Package["php5"],
   notify  => Package["apache2"]
 }
 
@@ -118,4 +146,94 @@ class { 'composer':
     Package['php5-suhosin'],
     Package['php5-cli']
   ]
+}
+
+# psysh
+exec { "composer install psysh":
+  command => 'su vagrant -c "composer g require psy/psysh:@stable"',
+  onlyif  => "test ! -e /home/vagrant/.composer/vendor/bin/",
+  require => Class["composer"]
+}
+
+file { "/home/vagrant/.psysh":
+  ensure => directory,
+  owner => "vagrant",
+  group => "vagrant",
+}
+
+exec { "psysh php manual":
+  command => "wget -O /home/vagrant/.psysh/php_manual.sqlite http://psysh.org/manual/en/php_manual.sqlite",
+  onlyif  => "test ! -e /home/vagrant/.psysh/php_manual.sqlite",
+  require => [
+    Exec["composer install psysh"],
+    File["/home/vagrant/.psysh"]
+  ]
+}
+class { "mailcatcher":
+  smtp_ip => "0.0.0.0",
+  http_ip => "0.0.0.0",
+}
+
+# gems
+class gems {
+  $gems = [
+    { name => "compass",      version => "latest" },
+    { name => "sass",         version => "latest" },
+    { name => "less",         version => "latest" },
+    { name => "therubyracer", version => "latest" },
+  ]
+
+  define install_gem {
+    package { $name[name]:
+      provider => "gem",
+      ensure   => $name[version],
+    }
+  }
+
+  install_gem {$gems: }
+}
+
+class {"gems":
+  stage => post
+}
+
+# node
+class {"nodejs": } ->
+package { "nodejs-legacy":
+  ensure => present,
+}
+
+package { "augeas-tools":
+  ensure => present,
+}
+
+package { "libaugeas-ruby":
+  ensure => present,
+}
+
+class conf {
+  augeas { "php-errors":
+    changes => [
+      "set /files/etc/php5/apache2/php.ini/PHP/display_errors On",
+      "set /files/etc/php5/cli/php.ini/PHP/display_errors On",
+    ],
+  }
+
+  augeas { "php-time":
+    changes => [
+      "set /files/etc/php5/apache2/php.ini/PHP/date.timezone Europe/Vilnius",
+      "set /files/etc/php5/cli/php.ini/PHP/date.timezone Europe/Vilnius",
+    ],
+    require => Package["augeas-tools"]
+  }
+
+  exec { "composer-vendor-path":
+    command => "echo 'export PATH=\$PATH:/home/vagrant/.composer/vendor/bin' >> /home/vagrant/.bashrc; touch /home/vagrant/.composer-path",
+    creates => "/home/vagrant/.composer-path",
+    user    => "vagrant"
+  }
+}
+
+class {"conf":
+  stage => post
 }
