ce6fdd7c18d40c51eaf1e5a615ad9b483b220367
einiusb <einiusb@gmail.com>
2014-10-29 12:29:54 +0200

    Added database SQL file

---
+++ SQL/atotrukis.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 29, 2014 at 12:26 PM
+-- Server version: 5.5.38
+-- PHP Version: 5.6.2-1~dotdeb.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `atotrukis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE IF NOT EXISTS `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `priority` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime NOT NULL,
+  `createdOn` datetime NOT NULL,
+  `map` varchar(2083) COLLATE utf8_unicode_ci NOT NULL,
+  `createdBy` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_5387574A2D5B0234` (`city`),
+  KEY `IDX_5387574AD3564642` (`createdBy`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_photos`
+--
+
+CREATE TABLE IF NOT EXISTS `event_photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `eventId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_3AEF978B2B2EBB6C` (`eventId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_1483A5E992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_1483A5E9A0D96FBF` (`email_canonical`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_attending`
+--
+
+CREATE TABLE IF NOT EXISTS `users_attending` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `eventId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_5E80E51664B64DCC` (`userId`),
+  KEY `IDX_5E80E5162B2EBB6C` (`eventId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_interests`
+--
+
+CREATE TABLE IF NOT EXISTS `user_interests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `keyword` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `valueFacebook` int(11) NOT NULL DEFAULT '0',
+  `valueGoogle` int(11) NOT NULL DEFAULT '0',
+  `valueOurPage` int(11) NOT NULL DEFAULT '0',
+  `userId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_C854880E64B64DCC` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `FK_5387574A2D5B0234` FOREIGN KEY (`city`) REFERENCES `cities` (`id`),
+  ADD CONSTRAINT `FK_5387574AD3564642` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `event_photos`
+--
+ALTER TABLE `event_photos`
+  ADD CONSTRAINT `FK_3AEF978B2B2EBB6C` FOREIGN KEY (`eventId`) REFERENCES `events` (`id`);
+
+--
+-- Constraints for table `users_attending`
+--
+ALTER TABLE `users_attending`
+  ADD CONSTRAINT `FK_5E80E5162B2EBB6C` FOREIGN KEY (`eventId`) REFERENCES `events` (`id`),
+  ADD CONSTRAINT `FK_5E80E51664B64DCC` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `user_interests`
+--
+ALTER TABLE `user_interests`
+  ADD CONSTRAINT `FK_C854880E64B64DCC` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
