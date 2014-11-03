2fdb7716680b7a78f4d9e2e15cddaf3513ee47b8
Aistė <aiste.zagorskyte@ktu.edu>
2014-11-03 20:13:15 +0200

    Changed date format to lithuanian

--- src/Atotrukis/MainBundle/Controller/DefaultController.php
+++ src/Atotrukis/MainBundle/Controller/DefaultController.php
@@ -21,21 +21,24 @@ class DefaultController extends Controller
 //        $city = new City(); $city->setName("Panevėžys"); $city->setPriority(1); $em = $this->getDoctrine()->getManager(); $em->persist($city); $em->flush();
 
         // Generating events
-//        $names = array("Koncertas", "Miesto šventė", "Parduotuvės atidarymas", "Futbolo varžybos", "Krepšinio varžybos",
-//                      "Šokiai", "Paroda", "Karaoke", "Protmūšis", "Spektaklis", "Paskaita", "Festivalis", "Muzikos vakaras");
-//        $descriptions = array("Labai įdomus renginys", "Linksmas laiko praleidimas", "Įdomu", "Smagu", "Čia toks labai įdomus renginys, kuriame pamatysite visokių įdomių dalykų, kurie bus labai smagūs ir linksmi.");
-//        $em = $this->getDoctrine()->getManager();
-//        $cities = $em->getRepository('AtotrukisMainBundle:City')->findAll();
-//        $event = new Event();
-//        $event->setName($names[array_rand($names, 1)]);
-//        $event->setDescription($descriptions[array_rand($descriptions, 1)]);
-//        $event->setStartDate(new \DateTime("2014-12-17 15:00"));
-//        $event->setEndDate(new \DateTime("2014-12-17 17:00"));
-//        $event->setCity($cities[array_rand($cities, 1)]);
-//        $event->setMap("https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2293.942924457549!2d23.957789!3d54.90392399999999!3m2!1i1024!2i768!4f13.1!5e0!3m2!1slt!2slt!4v1414796027316");
-//        $em = $this->getDoctrine()->getManager();
-//        $em->persist($event);
-//        $em->flush();
+//        for ($i = 0; $i < 10; $i++) {
+//            $names = array("Koncertas", "Miesto šventė", "Parduotuvės atidarymas", "Futbolo varžybos", "Krepšinio varžybos",
+//                "Šokiai", "Paroda", "Karaoke", "Protmūšis", "Spektaklis", "Paskaita", "Festivalis", "Muzikos vakaras");
+//            $descriptions = array("Labai įdomus renginys", "Linksmas laiko praleidimas", "Įdomu", "Smagu", "Čia toks labai įdomus renginys, kuriame pamatysite visokių įdomių dalykų, kurie bus labai smagūs ir linksmi.");
+//            $em = $this->getDoctrine()->getManager();
+//            $cities = $em->getRepository('AtotrukisMainBundle:City')->findAll();
+//            $dates = array("2014-11-20 15:00", "2014-12-01 18:00", "2015-01-01 20:20", "2014-12-12 15:00", "2015-01-15 20:00");
+//            $event = new Event();
+//            $event->setName($names[array_rand($names, 1)]);
+//            $event->setDescription($descriptions[array_rand($descriptions, 1)]);
+//            $event->setStartDate(new \DateTime($dates[array_rand($dates, 1)]));
+//            $event->setEndDate(new \DateTime("2016-10-10"));
+//            $event->setCity($cities[array_rand($cities, 1)]);
+//            $event->setMap("https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2293.942924457549!2d23.957789!3d54.90392399999999!3m2!1i1024!2i768!4f13.1!5e0!3m2!1slt!2slt!4v1414796027316");
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($event);
+//            $em->flush();
+//        }
 
         // Deleting all cities
 //        $em = $this->getDoctrine()->getManager();
@@ -55,6 +58,23 @@ class DefaultController extends Controller
 
 
         // -----------indexAction code------------------------
+
+        // Changing date format to lithuanian
+        function changeDate($time){
+
+            $mon = $time->format('n');
+            $months = array("Sausio", "Vasario", "Kovo", "Balandžio", "Gegužės", "Birželio",
+                "Liepos","Rugpjūčio", "Rugsėjo","Spalio", "Lapkričio", "Gruodžio");
+            $date = "";
+            if ($time->format('Y') != (new \DateTime())->format('Y')) {
+                $date .= $time->format('Y \m. ');
+            }
+            $date .= $months[$mon-1];
+            $date .= $time->format(' j \d. H:i');
+            return $date;
+
+        }
+
         // Getting city from ip address
         function ip_details($ip) {
             $json = file_get_contents("http://ipinfo.io/");
@@ -76,12 +96,17 @@ class DefaultController extends Controller
 
         $events = $query->getResult();
 
+        $date = [];
+        foreach ($events as $e) {
+            $date[$e->getId()] = changeDate($e->getStartDate());
+        }
+
         if (!$events) {
             throw $this->createNotFoundException('Nėra nei vieno renginio');
         }
 
         return $this->render('AtotrukisMainBundle:Default:index.html.twig', array(
-            'events' => $events
+            'events' => $events, 'date' => $date
         ));
     }
 
