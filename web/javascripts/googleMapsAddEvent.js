37ef967e8c718254528389dee3a9a972ed58ed7d
einiusb <einiusb@gmail.com>
2014-11-13 23:15:25 +0200

    Separated js and css from twig

---
+++ web/javascripts/googleMapsAddEvent.js
@@ -0,0 +1,91 @@
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+function initialize() {
+
+    var markers = [];
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(56.256216, 21.246148),
+        new google.maps.LatLng(53.943853, 25.937310));
+    map.fitBounds(defaultBounds);
+
+    // Create the search box and link it to the UI element.
+    var input = /** @type {HTMLInputElement} */(
+        document.getElementById('pac-input'));
+    //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var searchBox = new google.maps.places.SearchBox(
+        /** @type {HTMLInputElement} */(input));
+
+    // [START region_getplaces]
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+        for (var i = 0, marker; marker = markers[i]; i++) {
+            marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location,
+            });
+            document.getElementById("createEventForm_map").value = marker.position;
+            //alert(document.getElementById("createEventForm_map").value);
+            //$("#createEventForm_map").val(marker.position);
+            //alert(marker.position);
+
+            //markers = [];
+            markers.push(marker);
+            bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
+        map.setZoom(16);
+    });
+    // [END region_getplaces]
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+
+        //map.setCenter(pt);
+
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function stopRKey(evt) {
+    var evt = (evt) ? evt : ((event) ? event : null);
+    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+    if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+}
+
+document.onkeypress = stopRKey;
