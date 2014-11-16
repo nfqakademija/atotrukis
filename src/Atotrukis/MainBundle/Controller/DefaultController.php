2096d93939c530d1cb509349df78fad23ba4c42c
Aistė <aiste.zagorskyte@ktu.edu>
2014-11-16 13:18:38 +0200

    Working date format chenger

--- src/Atotrukis/MainBundle/Controller/DefaultController.php
+++ src/Atotrukis/MainBundle/Controller/DefaultController.php
@@ -3,38 +3,13 @@
 namespace Atotrukis\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Atotrukis\MainBundle\Entity\UserAttending;
 
 class DefaultController extends Controller
 {
 
     public function indexAction()
     {
-        
-        // Getting city from ip address
-//        $ip = $_SERVER['REMOTE_ADDR'];
-//        $json = file_get_contents("http://ipinfo.io/");
-//        $details = json_decode($json);
-//        if (!$details->city) {
-//            $json = file_get_contents("http://ipinfo.io/$ip");
-//            $details = json_decode($json);
-//        }
-//        $city = $details->city;
-
-
-        // Searching events according to city
         $em = $this->getDoctrine()->getManager();
-
-//        $qb = $em->createQueryBuilder()
-//            ->select('e')
-//            ->from('AtotrukisMainBundle:Event', 'e')
-//            ->innerJoin('e.city', 'c', 'WITH', 'e.city = c.id')
-//            ->where('e.startDate >= :today')
-//            ->andWhere('c.name = :city')
-//            ->setParameter('today', new \DateTime())
-//            ->setParameter('city', $city)
-//        ;
-//        $events = $qb->getQuery()->getResult();
 
         $qb = $em->createQueryBuilder()
             ->select('e')
@@ -45,28 +20,11 @@ class DefaultController extends Controller
         $events = $qb->getQuery()->getResult();
 
         if (!$events) {
-            // if city isn't got
-            $qb = $em->createQueryBuilder()
-                ->select('e')
-                ->from('AtotrukisMainBundle:Event', 'e')
-                ->where('e.startDate >= :today')
-                ->setParameter('today', new \DateTime())
-            ;
-            $events = $qb->getQuery()->getResult();
-
-            if (!$events) {
-                //TODO not a solution
-               // throw $this->createNotFoundException('Nėra nei vieno renginio');
-            }
+            //TODO not a solution
+           // throw $this->createNotFoundException('Nėra nei vieno renginio')
         }
 
-
-        // Changing date format to lithuanian
-        $date = [];
-        foreach ($events as $e) {
-            $date[$e->getId()] = changeDate($e->getStartDate());
-        }
-
+        $date = $this->get('dateFormatService')->dateArray($events);
 
         // Pagination
         $paginator  = $this->get('knp_paginator');
@@ -75,7 +33,6 @@ class DefaultController extends Controller
             $this->get('request')->query->get('puslapis', 1)/*page number*/,
             8/*limit per page*/
         );
-
 
         return $this->render('AtotrukisMainBundle:Default:index.html.twig', array(
             'date' => $date, 'pagination' => $pagination
