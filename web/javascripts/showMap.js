638bb09e9ac67ffaf1173d35261e2a229abb42ab
Aistė <aiste.zagorskyte@ktu.edu>
2014-11-22 16:51:07 +0200

    Added ability to see map of event

---
+++ web/javascripts/showMap.js
@@ -0,0 +1,24 @@
+function initialize(lat, lng) {
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: new google.maps.LatLng(lat, lng) ,
+        zoom: 16
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(request, function(place, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', function(){initialize(lat, lng);});
