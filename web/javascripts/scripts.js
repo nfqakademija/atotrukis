175db888ce3bfbe1377a7c1555e541fae78a1dcc
Aistė <aiste.zagorskyte@ktu.edu>
2014-11-28 21:02:28 +0200

    Moved js code to scripts.js, changed css files to sass and imported to style.sass

--- web/javascripts/scripts.js
+++ web/javascripts/scripts.js
@@ -65,3 +65,48 @@ $(".attendmentButton").on("click", ".attendingButton-sml", function(){
     });
 });
 $('[data-toggle="tooltip"]').tooltip();
+
+// quiz steps
+var current, next, previous;
+$(".next").click(function() {
+    current = $(this).parent().parent();
+    next = $(this).parent().parent().next();
+    next.show();
+    current.hide();
+});
+$(".previous").click(function() {
+    current = $(this).parent().parent();
+    previous = $(this).parent().parent().prev();
+    previous.show();
+    current.hide();
+});
+
+function initialize(lat, lng) {
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: new google.maps.LatLng(lat, lng) ,
+        zoom: 16
+    });
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map,
+    });
+    marker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(request, function(place, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
+    });
+}
+google.maps.event.addDomListener(window, 'load', function(){initialize(lat, lng);});
