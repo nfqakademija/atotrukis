daa50c3de19bc19665fdc3bfc8768c4d9181d5c8
gediminas <zarokas@gmail.com>
2014-12-08 12:13:40 +0200

    test fix

--- src/Atotrukis/MainBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Atotrukis/MainBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Brand")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Atotrūkis")')->count() > 0);
     }
 }
